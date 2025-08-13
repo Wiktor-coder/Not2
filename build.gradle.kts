39e8de98e959e833b9817cc4b6440f82446098cf
Wiktor Chumakov <Apostal333@gmail.com>
2025-08-13 08:56:40 +0300

    initional commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("jacoco")
+    kotlin("jvm") version "2.1.21"
+}
+
+group = "ru.netology"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("org.jetbrains.kotlin:kotlin-test")
+//    testImplementation(kotlin("test"))
+    testImplementation ("junit:junit:4.13.2")
+}
+
+tasks.test {
+    useJUnit()
+}
+kotlin {
+    jvmToolchain(13)
+}
